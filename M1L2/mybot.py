e78eb5529976615f901cf4913319304b43ddf255
Juan Aragón <juanm.aramas@gmail.com>
2026-02-07 13:50:19 +0100

    Add basic Discord bot functionality
    Implemented a Discord bot with commands for greeting, repeating 'he', and rolling dice.

---
+++ M1L2/mybot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hai fatto l\'accesso come {bot.user}')
+
+@bot.command()
+async def ciao(ctx):
+    await ctx.send(f'Ciao! Sono un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh, count2_heh):
+    await ctx.send("he" * int(count_heh) * int(count2_heh))
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+bot.run("")
