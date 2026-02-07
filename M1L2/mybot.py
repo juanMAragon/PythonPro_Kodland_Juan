403f1a5985d70e8196f35a89ec4ddd0d00cf3b34
Juan Aragón <juanm.aramas@gmail.com>
2026-02-07 13:52:52 +0100

    Remove roll command from mybot.py
    Removed the roll command and its functionality from the bot.

--- M1L2/mybot.py
+++ M1L2/mybot.py
@@ -20,17 +20,4 @@ async def heh(ctx, count_heh, count2_heh):
     await ctx.send("he" * int(count_heh) * int(count2_heh))
 
 
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
 bot.run("")
